37d83f35a930819e01be647740f9ead07660ea3c
cmddog <cmddog@users.noreply.github.com>
2025-12-06 03:06:40 +0100

    CLI, 2025 day 1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
+use inquire::Select;
+
+mod runner;
 mod y2015;
+mod y2025;
 
 fn main() {
-    println!("--------- 2015 ---------");
-    y2015::run();
+    let year = Select::new("Select year:", vec![2015, 2025])
+        .prompt()
+        .unwrap();
+
+    let days = match year {
+        2015 => y2015::available_days(),
+        2025 => y2025::available_days(),
+        _ => vec![],
+    };
+
+    let day = Select::new("Select day:", days)
+        .prompt()
+        .unwrap();
+
+    println!("\n========= {} Day {} =========", year, day);
+    match year {
+        2015 => y2015::run(day),
+        2025 => y2025::run(day),
+        _ => println!("Year not implemented"),
+    }
 }
---
+++ src/y2025/day1.rs
@@ -0,0 +1,28 @@
+use std::iter;
+
+pub fn part_1() -> usize {
+    let input = include_str!("day1.txt");
+    input
+        .lines()
+        .map(|s| s.replace("L", "-").replace("R", "").parse::<i32>().unwrap())
+        .scan(50, |acc, x| {
+            *acc = (*acc + x) % 100;
+            Some(*acc)
+        })
+        .filter(|&n| n == 0)
+        .count()
+}
+
+pub fn part_2() -> usize {
+    let input = include_str!("day1.txt");
+    input
+        .lines()
+        .map(|s| s.replace("L", "-").replace("R", "").parse::<i32>().unwrap())
+        .flat_map(|x| iter::repeat_n(x.signum(), x.abs() as usize))
+        .scan(50, |acc, x| {
+            *acc = (*acc + x) % 100;
+            Some(*acc)
+        })
+        .filter(|&n| n == 0)
+        .count()
+}
